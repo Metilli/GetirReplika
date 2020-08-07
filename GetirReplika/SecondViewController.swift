b9c7a76ae5a04c38c5d04e0a87ec67d5b184bbde
Metilli <emremetilli@gmail.com>
2020-08-07 13:10:08 +0300

    Initial Commit

---
+++ GetirReplika/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GetirReplika
+//
+//  Created by Metilli on 7.08.2020.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
